77e10b5306affd2408bf2c6c8c1f6b92eec9f9e1
WittySkates <connordupuis760@gmail.com>
2021-12-15 13:41:50 -0500

    Updates with only new changes from wiki

--- src/App.jsx
+++ src/App.jsx
@@ -8,65 +8,74 @@ import "./App.scss";
 const database = firebase.database();
 
 const App = () => {
-	const dayInMS = 86400000;
-	const [traderTrees, setTraderTrees] = useState({});
-	const [traderNames, setTraderNames] = useState([]);
-	const [currentTrader, setCurrentTrader] = useState(0);
+  const [traderTrees, setTraderTrees] = useState({});
+  const [traderNames, setTraderNames] = useState([]);
+  const [currentTrader, setCurrentTrader] = useState(0);
 
-	useEffect(() => {
-		(async () => {
-			if (
-				!localStorage.getItem("tarkov-tree") ||
-				Date.now() - Number(localStorage.getItem("tarkov-time")) >
-					3 * dayInMS
-			) {
-				localStorage.setItem("tarkov-time", Date.now().toString(10));
-				await database
-					.ref("traderTree")
-					.get()
-					.then((snapshot) => {
-						if (snapshot.exists()) {
-							localStorage.setItem("tarkov-tree", snapshot.val());
-						} else {
-							return null;
-						}
-					})
-					.catch((error) => {
-						console.log("Erroring getting trader tree" + error);
-					});
-			}
+  useEffect(() => {
+    (async () => {
+      let lastUpdated;
+      await database
+        .ref("traderQuests/lastUpdated")
+        .get()
+        .then(snapshot => {
+          if (snapshot.exists()) {
+            lastUpdated = snapshot.val();
+          } else {
+            return null;
+          }
+        })
+        .catch(error => {
+          console.log("Erroring getting lastUpdated" + error);
+        });
+      if (
+        !localStorage.getItem("tarkov-time") ||
+        localStorage.getItem("tarkov-time") !== lastUpdated.toString()
+      ) {
+        localStorage.setItem("tarkov-time", lastUpdated);
+        await database
+          .ref("traderTree")
+          .get()
+          .then(snapshot => {
+            if (snapshot.exists()) {
+              localStorage.setItem("tarkov-tree", snapshot.val());
+            } else {
+              return null;
+            }
+          })
+          .catch(error => {
+            console.log("Erroring getting trader tree" + error);
+          });
+      }
 
-			const traderTrees = JSON.parse(localStorage.getItem("tarkov-tree"));
-			setTraderTrees(traderTrees);
+      const traderTrees = JSON.parse(localStorage.getItem("tarkov-tree"));
+      setTraderTrees(traderTrees);
 
-			const traderNames = _.reduce(
-				traderTrees,
-				(acc, entry) => [
-					...acc,
-					{ name: entry.name, attributes: entry.attributes },
-				],
-				[]
-			);
-			setTraderNames(traderNames);
-		})();
-	}, []);
+      const traderNames = _.reduce(
+        traderTrees,
+        (acc, entry) => [...acc, { name: entry.name, attributes: entry.attributes }],
+        []
+      );
+      setTraderNames(traderNames);
+    })();
+  }, []);
 
-	return (
-		<>
-			<TopNav />
-			<Traderbar
-				traderNames={traderNames}
-				traderTrees={traderTrees}
-				currentTrader={currentTrader}
-				setCurrentTrader={setCurrentTrader}
-			/>
-			<TraderTree
-				traderData={traderTrees[currentTrader]}
-				trader={traderNames[currentTrader]?.name}
-				traderIndex={currentTrader}
-			/>
-		</>
-	);
+  return (
+    <>
+      <TopNav />
+      <Traderbar
+        traderNames={traderNames}
+        traderTrees={traderTrees}
+        currentTrader={currentTrader}
+        setCurrentTrader={setCurrentTrader}
+      />
+      <TraderTree
+        traderData={traderTrees[currentTrader]}
+        trader={traderNames[currentTrader]?.name}
+        traderIndex={currentTrader}
+      />
+    </>
+  );
 };
 
 export default App;
