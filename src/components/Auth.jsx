bc2b2c0e97262be77f2252628e28caffebe38422
WittySkates <connordupuis760@gmail.com>
2021-12-15 13:45:24 -0500

    Config change before merge

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -15,74 +15,75 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 // CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-firebase.initializeApp(firebaseConfigDev);
+firebase.initializeApp(firebaseConfig);
 
 const Auth = () => {
-	const auth = firebase.auth();
-	const database = firebase.database();
-	const provider = new firebase.auth.GoogleAuthProvider();
-	const [user, loading, error] = useAuthState(auth);
+  const auth = firebase.auth();
+  const database = firebase.database();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const [user, loading, error] = useAuthState(auth);
 
-	const SignIn = () => {
-		auth.signInWithPopup(provider)
-			.then((result) => {
-				/** @type {firebase.auth.OAuthCredential} */
-				let credential = result.credential;
-				// This gives you a Google Access Token. You can use it to access the Google API.
-				let token = credential.accessToken;
-				// The signed-in user info.
-				let user = result.user;
+  const SignIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(result => {
+        /** @type {firebase.auth.OAuthCredential} */
+        let credential = result.credential;
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        let token = credential.accessToken;
+        // The signed-in user info.
+        let user = result.user;
 
-				database.ref("users/" + user.uid).update({
-					uid: user.uid,
-					name: user.displayName,
-					email: user.email,
-					creationTime: user.metadata.creationTime,
-				});
-			})
-			.catch((error) => {
-				// Handle Errors here.
-				let errorCode = error.code;
-				let errorMessage = error.message;
-				// The email of the user's account used.
-				let email = error.email;
-				// The firebase.auth.AuthCredential type that was used.
-				let credential = error.credential;
-			});
-	};
-	if (error) {
-		console.log(error);
-		return (
-			<div>
-				<p>Couldn't Log You in: {error}</p>
-			</div>
-		);
-	}
-	if (loading) {
-		return (
-			<div>
-				<p>...Initializing User</p>
-			</div>
-		);
-	}
-	if (user) {
-		return (
-			<Button
-				className="tarkov-auth-signout"
-				variant="outline-danger"
-				onClick={() => {
-					auth.signOut();
-				}}
-			>
-				Sign Out
-			</Button>
-		);
-	}
-	return (
-		<Button variant="primary" onClick={() => SignIn()}>
-			Sign In With Google
-		</Button>
-	);
+        database.ref("users/" + user.uid).update({
+          uid: user.uid,
+          name: user.displayName,
+          email: user.email,
+          creationTime: user.metadata.creationTime
+        });
+      })
+      .catch(error => {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // The email of the user's account used.
+        let email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        let credential = error.credential;
+      });
+  };
+  if (error) {
+    console.log(error);
+    return (
+      <div>
+        <p>Couldn't Log You in: {error}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return (
+      <div>
+        <p>...Initializing User</p>
+      </div>
+    );
+  }
+  if (user) {
+    return (
+      <Button
+        className="tarkov-auth-signout"
+        variant="outline-danger"
+        onClick={() => {
+          auth.signOut();
+        }}
+      >
+        Sign Out
+      </Button>
+    );
+  }
+  return (
+    <Button variant="primary" onClick={() => SignIn()}>
+      Sign In With Google
+    </Button>
+  );
 };
 
 export default Auth;
