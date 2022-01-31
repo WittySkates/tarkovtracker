e43ae4ef0391ce0980d3fce218a05c3a3ad6cdfb
WittySkates <connordupuis760@gmail.com>
2022-01-31 04:08:30 -0500

    Animated selector. (kinda janky)

--- src/components/GeneralNav/GeneralNav.jsx
+++ src/components/GeneralNav/GeneralNav.jsx
@@ -1,15 +1,17 @@
 /** @module GeneralNav */
 
-import React from "react";
+import React, { useEffect } from "react";
+import $ from "jquery";
 import "./styles/generalnav.scss";
 
 const GeneralNav = props => {
   const { navArray, className, setCurrentValue, currentValue } = props;
+
   return (
     <div className={`generalNav ${className}`}>
       {navArray.map((map, index) => (
         <div
-          className={`navElement ${currentValue === map && "navSelected"}`}
+          className={`navElement ${currentValue === map && "navSelected"} ${currentValue}`}
           onClick={() => {
             setCurrentValue(map);
           }}
@@ -17,6 +19,7 @@ const GeneralNav = props => {
           {map}
         </div>
       ))}
+      <div className="navSlider" />
     </div>
   );
 };
