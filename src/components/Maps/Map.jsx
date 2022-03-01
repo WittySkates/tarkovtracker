ac3378f086e44dba371874d5a53be7d905ea1f67
WittySkates <connordupuis760@gmail.com>
2022-03-01 01:05:49 -0500

    Finally added new maps

--- src/components/Maps/Map.jsx
+++ src/components/Maps/Map.jsx
@@ -4,27 +4,36 @@ import React, { useEffect, useRef, useState } from "react";
 import Minature from "./MapTools/Minature";
 import Toolbar from "./MapTools/Toolbar";
 
-import customsJPG from "./mapimages/Customs/customs.jpg";
+import customs from "./mapimages/Customs/customs.jpg";
+import customsGray from "./mapimages/Customs/customsGray.jpg";
 import customsSVG from "./mapimages/Customs/Customs.svg";
 
-import shorelineJPG from "./mapimages/Shoreline/shoreline.jpg";
+import shoreline from "./mapimages/Shoreline/shoreline.jpg";
+import shorelineSpawn from "./mapimages/Shoreline/shorelineSpawn.jpg";
 import shorelineSVG from "./mapimages/Shoreline/Shoreline.svg";
 
-import factoryJPG from "./mapimages/Factory/factory.jpg";
+import factory from "./mapimages/Factory/factory.jpg";
 import factorySVG from "./mapimages/Factory/Factory.svg";
 
-import woodsJPG from "./mapimages/Woods/woods.jpg";
+import woods from "./mapimages/Woods/woods.jpg";
 import woodsSVG from "./mapimages/Woods/Woods.svg";
 
-import reserveJPG from "./mapimages/Reserve/reserve.jpg";
+import reserve from "./mapimages/Reserve/reserve.jpg";
+import reserveGray from "./mapimages/Reserve/reserveGray.jpg";
+import reserveKey from "./mapimages/Reserve/reserveKey.jpg";
 import reserveSVG from "./mapimages/Reserve/Reserve.svg";
 
 import interchangeJPG from "./mapimages/Interchange/interchange.jpg";
 import interchangeSVG from "./mapimages/Interchange/Interchange.svg";
 
-import lighthouseMap from "./mapimages/Lighthouse/lighthouse.jpg";
+import lighthouse from "./mapimages/Lighthouse/lighthouse.jpg";
+import lighthouseRotated from "./mapimages/Lighthouse/lighthouseRotated.jpg";
+import lighthouseFlat from "./mapimages/Lighthouse/lighthouseFlat.jpg";
 
-import labsMap from "./mapimages/Labs/labs.jpg";
+import labs from "./mapimages/Labs/labs.jpg";
+import labsExtract from "./mapimages/Labs/labsExtract.jpg";
+import labsHorizontal from "./mapimages/Labs/labsHorizontal.jpg";
+import labsVertical from "./mapimages/Labs/labsVertical.jpg";
 
 import { UncontrolledReactSVGPanZoom } from "react-svg-pan-zoom";
 import { colors } from "../../constants";
@@ -33,31 +42,44 @@ import "./styles/map.scss";
 
 const allMaps = {
   Customs: [
-    { map: customsJPG, about: "3D" },
+    { map: customs, about: "3D" },
+    { map: customsGray, about: "3D" },
     { map: customsSVG, about: "2D" }
   ],
   Shoreline: [
-    { map: shorelineJPG, about: "3D" },
+    { map: shoreline, about: "3D" },
+    { map: shorelineSpawn, about: "3D" },
     { map: shorelineSVG, about: "2D" }
   ],
   Factory: [
-    { map: factoryJPG, about: "3D" },
+    { map: factory, about: "3D" },
     { map: factorySVG, about: "2D" }
   ],
   Woods: [
-    { map: woodsJPG, about: "3D" },
+    { map: woods, about: "3D" },
     { map: woodsSVG, about: "2D" }
   ],
   Reserve: [
-    { map: reserveJPG, about: "3D" },
+    { map: reserve, about: "3D" },
+    { map: reserveGray, about: "3D" },
+    { map: reserveKey, about: "3D" },
     { map: reserveSVG, about: "2D" }
   ],
   Interchange: [
     { map: interchangeJPG, about: "3D" },
     { map: interchangeSVG, about: "2D" }
   ],
-  Lighthouse: [{ map: lighthouseMap, about: "3D" }],
-  Labs: [{ map: labsMap, about: "3D" }]
+  Lighthouse: [
+    { map: lighthouse, about: "3D" },
+    { map: lighthouseRotated, about: "3D" },
+    { map: lighthouseFlat, about: "3D" }
+  ],
+  Labs: [
+    { map: labs, about: "3D" },
+    { map: labsHorizontal, about: "3D" },
+    { map: labsVertical, about: "3D" },
+    { map: labsExtract, about: "3D" }
+  ]
 };
 
 const Map = props => {
