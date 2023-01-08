8cd2037cef8bddd9252f8d57cc5fa77693334690
Connor <connordupuis760@gmail.com>
2023-01-07 23:23:11 -0700

    Added sub maps

--- src/pages/Maps.tsx
+++ src/pages/Maps.tsx
@@ -1,26 +1,21 @@
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import customs from "../mapimages/Customs/customs.jpg";
 import customsGray from "../mapimages/Customs/customsGray.jpg";
-import customsSVG from "../mapimages/Customs/Customs.svg";
 
 import shoreline from "../mapimages/Shoreline/shoreline.jpg";
 import shorelineSpawn from "../mapimages/Shoreline/shorelineSpawn.jpg";
-import shorelineSVG from "../mapimages/Shoreline/Shoreline.svg";
 
 import factory from "../mapimages/Factory/factory.jpg";
 import factorySVG from "../mapimages/Factory/Factory.svg";
 
 import woods from "../mapimages/Woods/woods.jpg";
-import woodsSVG from "../mapimages/Woods/Woods.svg";
 
 import reserve from "../mapimages/Reserve/reserve.jpg";
 import reserveGray from "../mapimages/Reserve/reserveGray.jpg";
 import reserveKey from "../mapimages/Reserve/reserveKey.jpg";
-import reserveSVG from "../mapimages/Reserve/Reserve.svg";
 
 import interchangeJPG from "../mapimages/Interchange/interchange.jpg";
-import interchangeSVG from "../mapimages/Interchange/Interchange.svg";
 
 import lighthouse from "../mapimages/Lighthouse/lighthouse.jpg";
 import lighthouseRotated from "../mapimages/Lighthouse/lighthouseRotated.jpg";
@@ -51,34 +46,34 @@ const mapNames: string[] = [
     "Labs",
 ];
 
-export interface LocationMaps {}
-
 const tarkovMaps = new Map<string, string[]>()
-    .set("Customs", [customs, customsGray, customsSVG])
-    .set("Shoreline", [shoreline, shorelineSpawn, shorelineSVG])
-    .set("Factory", [factory, factorySVG])
-    .set("Woods", [woods, woodsSVG])
-    .set("Reserve", [reserve, reserveGray, reserveSVG, reserveKey])
-    .set("Interchange", [interchangeJPG, interchangeSVG])
+    .set("Customs", [customs, customsGray])
+    .set("Shoreline", [shoreline, shorelineSpawn])
+    .set("Factory", [factory])
+    .set("Woods", [woods])
+    .set("Reserve", [reserve, reserveGray, reserveKey])
+    .set("Interchange", [interchangeJPG])
     .set("Lighthouse", [lighthouse, lighthouseRotated, lighthouseFlat])
     .set("Labs", [labs, labsExtract, labsHorizontal, labsVertical]);
 
 const getTarkovMaps = (mapName: string) => {
     const map = tarkovMaps.get(mapName);
     if (!map) {
-        return "Unknown Map";
+        return [];
     }
     return map;
 };
 
 const Maps = () => {
     const [currentMap, setCurrentMap] = useState<number>(0);
+    const [currentSubMap, setCurrentSubMap] = useState<number>(0);
     const ref = useRef<ReactZoomPanPinchRef>(null);
 
     useEffect(() => {
         setTimeout(() => {
             ref?.current?.centerView(0.5, 500, "easeOut");
         }, 300);
+        setCurrentSubMap(0);
     }, [currentMap]);
 
     return (
@@ -88,7 +83,16 @@ const Maps = () => {
                 currentNav={currentMap}
                 setCurrentNav={setCurrentMap}
             />
-            <div className="element">
+            <GenericNavbar
+                navData={getTarkovMaps(mapNames[currentMap]).map(
+                    (map, index) => {
+                        return (index + 1).toString();
+                    }
+                )}
+                currentNav={currentSubMap}
+                setCurrentNav={setCurrentSubMap}
+            />
+            <div className="image-wrapper">
                 <TransformWrapper
                     ref={ref}
                     minScale={0.1}
@@ -96,10 +100,14 @@ const Maps = () => {
                     centerZoomedOut
                     centerOnInit
                 >
-                    <TransformComponent wrapperClass="wrapper">
+                    <TransformComponent wrapperClass="transform-component">
                         <img
                             width="100%"
-                            src={getTarkovMaps(mapNames[currentMap])[0]}
+                            src={
+                                getTarkovMaps(mapNames[currentMap])[
+                                    currentSubMap
+                                ]
+                            }
                             alt={mapNames[currentMap]}
                             loading="eager"
                         />
